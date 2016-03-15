cad5b411cfe18e3808f3f772c6be40592df20eaa
Naor Ben David <naor.bendavid@thetaray.com>
2016-03-15 14:18:13 +0200

    MVP-1847: default path to csvGen

--- src/main/java/com/tr/csvgenerator/GenerateDataForSupervised/GenerateDataForSupervisedImpl.java
+++ src/main/java/com/tr/csvgenerator/GenerateDataForSupervised/GenerateDataForSupervisedImpl.java
@@ -33,8 +33,8 @@ public class GenerateDataForSupervisedImpl implements GenerateDataForSupervised 
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
         Date date = new Date();
-        String FilePath = System.getProperty("user.dir") + File.pathSeparator + dateFormat.format(date) + "_SP_DataSet_(" + Total_Size + ").csv";
-
+        //String FilePath = System.getProperty("user.dir") + File.pathSeparator + dateFormat.format(date) + "_SP_DataSet_(" + Total_Size + ").csv";
+        String FilePath = "/opt/tr/input/csvgen/output" + File.pathSeparator + dateFormat.format(date) + "_SP_DataSet_(" + Total_Size + ").csv";
 
         /*  Transform params for general to specific */
         int Train_Pos_Size = Total_Size * Positive_Percent / 100;
